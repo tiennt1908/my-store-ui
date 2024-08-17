1225eb336ee953de3fabd9f69ec713cab4b6b07b
MSI\MSII <tiennt1908@gmail.com>
2024-08-17 12:53:13 +0700

    handle render cart empty

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -7,6 +7,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import CartInfo from './CartInfo';
 import Item from './Item';
 import TotalCart from './TotalCart';
+import RenderIf from '@/components/RenderIf';
+import { ArchiveBoxXMarkIcon } from '@heroicons/react/24/outline';
+import WrappedLink from '@/components/Wrapped/WrappedLink';
+import Button from '@/components/Button';
 
 type Props = {};
 
@@ -28,19 +32,36 @@ export default function CartPage({}: Props) {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-lg font-medium">Giỏ hàng của bạn</h1>
-      <div className="grid grid-cols-12 gap-4">
-        <div className="col-span-8">
-          <div className="bg-white rounded shadow-sm" style={{ minHeight: 180 * 3 }}>
-            {cart.detail.list.map((p) => {
-              return <Item key={p.id} {...p} />;
-            })}
+      <RenderIf isRender={!cart.detail.isLoading}>
+        <RenderIf isRender={cart.detail.list.length > 0}>
+          <div className="grid grid-cols-12 gap-4">
+            <div className="col-span-8">
+              <div className="bg-white rounded shadow-sm" style={{ minHeight: 180 * 2 }}>
+                {cart.detail.list.map((p) => {
+                  return <Item key={p.id} {...p} />;
+                })}
+              </div>
+            </div>
+            <div className="col-span-4 flex flex-col gap-4">
+              <TotalCart products={cart.detail.list} mapItems={cart.mapItems} />
+              <CartInfo orderItems={orderItems} />
+            </div>
           </div>
-        </div>
-        <div className="col-span-4 flex flex-col gap-4">
-          <TotalCart products={cart.detail.list} mapItems={cart.mapItems} />
-          <CartInfo orderItems={orderItems} />
-        </div>
-      </div>
+        </RenderIf>
+        <RenderIf isRender={cart.detail.list.length === 0}>
+          <div className="rounded bg-white shadow-sm flex justify-center items-center" style={{ height: 3 * 180 }}>
+            <div className="flex flex-col justify-center items-center gap-2">
+              <ArchiveBoxXMarkIcon className="w-14 text-slate-300" />
+              <p>Bạn chưa có sản phẩm nào</p>
+              <WrappedLink href="/products">
+                <Button theme="black" width={150}>
+                  Mua ngay
+                </Button>
+              </WrappedLink>
+            </div>
+          </div>
+        </RenderIf>
+      </RenderIf>
     </div>
   );
 }
